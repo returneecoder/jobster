2a23ce206b63b5b83fbf15aacca772ce7287ea31
returneecoder <akhtar.amna81@gmail.com>
2023-06-18 08:37:16 +0400

    landing page complete

---
+++ src/pages/Landing.js
@@ -0,0 +1,64 @@
+import main from '../assets/images/main.svg';
+import logo from '../assets/images/logo.svg'
+import styled from 'styled-components'
+
+const Landing = () => {
+  return (
+   <Wrapper>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>'m baby ascot vape kitsch la croix waistcoat pour-over vegan butcher disrupt tonx jawn. Twee palo santo cloud bread put a bird on it, vegan next level lomo jawn. Heirloom fit organic adaptogen pinterest slow-carb wayfarers austin hexagon narwhal, blue bottle hella shoreditch vinyl mukbang. Fam cardigan pork belly hell of.</p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+      </Wrapper>
+  );
+};
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`
+
+export default Landing
